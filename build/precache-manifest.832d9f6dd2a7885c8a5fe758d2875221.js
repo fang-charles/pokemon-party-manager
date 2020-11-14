e207714a2924704b3951b0ee255b0dc5e3d7f736
Jammie Wang <jw8kc@virginia.edu>
2020-11-14 00:37:43 -0500

    remove build from gitignore

---
+++ build/precache-manifest.832d9f6dd2a7885c8a5fe758d2875221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bba0e8c3b07364d61ec10705d41b020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "982ee541f57ecf87457a",
+    "url": "/static/css/main.9ea246cc.chunk.css"
+  },
+  {
+    "revision": "5ba480edc328658d0036",
+    "url": "/static/js/2.f3497ae8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.f3497ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982ee541f57ecf87457a",
+    "url": "/static/js/main.f3e0dd01.chunk.js"
+  },
+  {
+    "revision": "435d78de063bdf25aabf",
+    "url": "/static/js/runtime-main.d28ddd9b.js"
+  }
+]);
