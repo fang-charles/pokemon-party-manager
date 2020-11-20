16c25e37b28782c6a9f4c73ecf288400fa3f5ff2
fang-charles <35390997+fang-charles@users.noreply.github.com>
2020-11-20 00:55:05 -0500

    Updates

---
+++ precache-manifest.2dce40e9dac2b7e4171dc70cf88825b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c070ed7629c1dd3623260c525d28e0",
+    "url": "/pokemon-party-manager/index.html"
+  },
+  {
+    "revision": "199a2da89eee28982d5a",
+    "url": "/pokemon-party-manager/static/css/main.cf1bf63c.chunk.css"
+  },
+  {
+    "revision": "6621a81dd552a9ea6328",
+    "url": "/pokemon-party-manager/static/js/2.68b8742f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/pokemon-party-manager/static/js/2.68b8742f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199a2da89eee28982d5a",
+    "url": "/pokemon-party-manager/static/js/main.2ef41aa3.chunk.js"
+  },
+  {
+    "revision": "6bbe4d1a738f80437d98",
+    "url": "/pokemon-party-manager/static/js/runtime-main.151ada69.js"
+  }
+]);
